e9254536d96a2d6aba3ecff3388edf9cf4059a43
Ayanfe Afolabi <afolabi.ayanfe@decagon.dev>
2023-10-29 06:53:47 +0100

    Complete forget password

---
+++ lib/screens/auth/create_new_password.dart
@@ -0,0 +1,18 @@
+
+
+
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class NewPasswordScreen extends StatefulHookConsumerWidget {
+  const NewPasswordScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _NewPasswordScreenState();
+}
+class _NewPasswordScreenState extends ConsumerState<NewPasswordScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/screens/auth/forget_password_screen.dart
+++ lib/screens/auth/forget_password_screen.dart
@@ -1,5 +1,17 @@
+import 'package:auto_adjust/auto_adjust.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:lottie/lottie.dart';
+import 'package:padi4life/utils/app_componenet/padded.dart';
+
+import '../../utils/app_componenet/padi4life_button.dart';
+import '../../utils/app_componenet/padi4life_textfield.dart';
+import '../../utils/constants.dart';
+import '../../utils/navigations.dart';
+import '../../utils/validator.dart';
+import 'create_new_password.dart';
 
 class ForgetPasswordScreen extends StatefulHookConsumerWidget {
   const ForgetPasswordScreen({super.key});
@@ -10,13 +22,78 @@ class ForgetPasswordScreen extends StatefulHookConsumerWidget {
 }
 
 class _ForgetPasswordScreenState extends ConsumerState<ForgetPasswordScreen> {
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
+    var emailMail = useTextEditingController();
     return Scaffold(
-      body: Column(
-        children: [
-          
-        ],
+      body: Padded(
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              Gap(autoAdjustHeight(56)),
+              Center(
+                child: Text(
+                  'Forget Password',
+                  style: kTextStyleCustom(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600,
+                    height: 0,
+                  ),
+                ),
+              ),
+              Gap(autoAdjustHeight(50)),
+              Center(
+                child: SizedBox(
+                  height: autoAdjustHeight(150),
+                  width: autoAdjustWidth(150),
+                  child: LottieBuilder.asset('assets/forgetpassword.json'),
+                ),
+              ),
+              Gap(autoAdjustHeight(50)),
+              Center(
+                child: Text(
+                  'To reset yor password, enter your email address and a token for verification will be sent to your email',
+                  textAlign: TextAlign.center,
+                  style: kTextStyleCustom(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    height: 0,
+                  ),
+                ),
+              ),
+              Gap(autoAdjustHeight(40)),
+              Padi4LifeTextFormField(
+                controller: emailMail,
+                hintText: 'Email address',
+                validator: (data) => Validator.validateEmail(data ?? ''),
+              ),
+              Gap(autoAdjustHeight(24)),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: EdgeInsets.only(bottom: autoAdjustHeight(60)),
+                    child: Padi4LifeMainButton(
+                      text: 'Send Token',
+                      width: double.maxFinite,
+                      onTap: () {
+                        if (formKey.currentState!.validate()) {
+                          RouteNavigators.route(
+                            context,
+                            const NewPasswordScreen(),
+                          );
+                        }
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
